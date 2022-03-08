e8fee704024d7d0df1252364661337bc24125b58
Febe Calderon <samcalma1@gmail.com>
2022-03-08 16:23:21 +0100

    errore apertura/chiusura menu risolto

--- main.js
+++ main.js
@@ -6,23 +6,19 @@ const closeIcon = document.querySelector(".close");
 
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 
-hamburgerIcon.addEventListener("click",
-  function () => {
-
-    // hamburgerMenu.style.display = 'block';
-    /*
+hamburgerIcon.addEventListener("click", function () {
+  // hamburgerMenu.style.display = 'block';
+  /*
     const classInElement = hamburgerMenu.className
     hamburgerMenu.className = classInElement + 'active';
 
     hamburgerMenu.className = hamburgerMenu.className + 'active'; 
   
-    hamburgerMenu.className = 'active';
+    hamburgerMenu.className += 'active';
     */
-    hamburgerMenu.classList('active');
+  hamburgerMenu.classList.add("active");
 });
 
-closeIcon.classListener('click',
-  function () {
-    hamburgerMenu.classList.remove('active');
-  }
-)
+closeIcon.addEventListener("click", function () {
+  hamburgerMenu.classList.remove("active");
+});
